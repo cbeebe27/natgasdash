87ef722f75892f8ef30c94f1fe94ebd31182a49e
cbeebe19 <156461417+cbeebe27@users.noreply.github.com>
2024-04-07 14:58:40 -0600

    data loaded in and cleaned

---
+++ R/data_documentation.R
@@ -0,0 +1,73 @@
+#' Gas Production Data (s1)
+#'
+#' This dataset contains annual gas production data in million cubic meters per day.
+#' The data was extracted from the 'ST98-NaturalGas-SupplyDemand-data.xlsx' Excel file.
+#' @format A data frame with 31 rows and 11 columns.
+#' @source Original data was sourced from [https://www.aer.ca/providing-information/data-and-reports/statistical-reports/st98/statistics-and-data].
+#' @name s1
+NULL
+
+
+#' Wells Placed On Production Data (s2)
+#'
+#' This dataset contains annual wells placed on production data, as well as marketable production in million cubic meters per day.
+#' The data was extracted from the 'ST98-NaturalGas-SupplyDemand-data.xlsx' Excel file.
+#' @format A data frame with 87 rows and 3 columns.
+#' @source Original data was sourced from [https://www.aer.ca/providing-information/data-and-reports/statistical-reports/st98/statistics-and-data].
+#' @name s2
+NULL
+
+#' Bitumen Well Data (s3)
+#'
+#' This dataset contains annual bitumen well data in million cubic meters per day.
+#' The data was extracted from the 'ST98-NaturalGas-SupplyDemand-data.xlsx' Excel file.
+#' @format A data frame with 33 rows and 3 columns.
+#' @source Original data was sourced from [https://www.aer.ca/providing-information/data-and-reports/statistical-reports/st98/statistics-and-data].
+#' @name s3
+NULL
+
+#' Gas Purchases Data (s4)
+#'
+#' This dataset contains annual gas purchases from different industries in million cubic meters per day.
+#' The data was extracted from the 'ST98-NaturalGas-SupplyDemand-data.xlsx' Excel file.
+#' @format A data frame with 33 rows and 7 columns.
+#' @source Original data was sourced from [https://www.aer.ca/providing-information/data-and-reports/statistical-reports/st98/statistics-and-data].
+#' @name s4
+NULL
+
+#' Shale Well Data (s5)
+#'
+#' This dataset contains annual shale well data in number of wells, as well as Alberta Plant gate price in Cdn$/GJ.
+#' The data was extracted from the 'ST98-NaturalGas-SupplyDemand-data.xlsx' Excel file.
+#' @format A data frame with 33 rows and 4 columns.
+#' @source Original data was sourced from [https://www.aer.ca/providing-information/data-and-reports/statistical-reports/st98/statistics-and-data].
+#' @name s5
+NULL
+
+#' Gas Demand Data (s7)
+#'
+#' This dataset contains annual gas demand  by source in million cubic meters per day.
+#' The data was extracted from the 'ST98-NaturalGas-SupplyDemand-data.xlsx' Excel file.
+#' @format A data frame with 33 rows and 10 columns.
+#' @source Original data was sourced from [https://www.aer.ca/providing-information/data-and-reports/statistical-reports/st98/statistics-and-data].
+#' @name s7
+NULL
+
+#' Gas Purchases Data (s9)
+#'
+#' This dataset contains annual land available for permitting, in billion cubic meters.
+#' The data was extracted from the 'ST98-NaturalGas-SupplyDemand-data.xlsx' Excel file.
+#' @format A data frame with 23 rows and 2 columns.
+#' @source Original data was sourced from [https://www.aer.ca/providing-information/data-and-reports/statistical-reports/st98/statistics-and-data].
+#' @name s9
+NULL
+
+#' Gas Purchases Data (s10)
+#'
+#' This dataset contains number of horizontal and vertical wells as well as production in million cubic meters per day.
+#' The data was extracted from the 'ST98-NaturalGas-SupplyDemand-data.xlsx' Excel file.
+#' @format A data frame with 33 rows and 5 columns.
+#' @source Original data was sourced from [https://www.aer.ca/providing-information/data-and-reports/statistical-reports/st98/statistics-and-data].
+#' @name s10
+NULL
+
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,29 +1,50 @@
 ## code to prepare `DATASET` dataset goes here
 
 library(readxl)
+library(dplyr)
+library(lubridate)
+library(tidyverse)
+
 
 s1 <- readxl::read_excel("C:/Users/conno/OneDrive/Desktop/Winter 24/FINTECH 3/natgastables.xlsx", sheet = 1)
+s1 <- s1 %>%  dplyr::mutate(Year =  lubridate::make_date(Year))
 usethis::use_data(s1, overwrite = T)
 
+
+
 s2 <-  readxl::read_excel("C:/Users/conno/OneDrive/Desktop/Winter 24/FINTECH 3/natgastables.xlsx", sheet = 2)
+s2 <- s2 %>%  dplyr::mutate(Year =  lubridate::make_date(Year))
 usethis::use_data(s2, overwrite = T)
 
+
+
 s3 <-  readxl::read_excel("C:/Users/conno/OneDrive/Desktop/Winter 24/FINTECH 3/natgastables.xlsx", sheet = 3)
+s3 <- s3 %>%  dplyr::mutate(Year =  lubridate::make_date(Year))
 usethis::use_data(s3, overwrite = T)
 
+
 s4 <-  readxl::read_excel("C:/Users/conno/OneDrive/Desktop/Winter 24/FINTECH 3/natgastables.xlsx", sheet = 4)
+s4 <- s4 %>%  dplyr::mutate(Year =  lubridate::make_date(Year))
 usethis::use_data(s4, overwrite = T)
 
+
 s5 <-  readxl::read_excel("C:/Users/conno/OneDrive/Desktop/Winter 24/FINTECH 3/natgastables.xlsx", sheet = 5)
+s5 <- s5 %>%  dplyr::mutate(Year =  lubridate::make_date(Year))
 usethis::use_data(s5, overwrite = T)
 
+
 s7 <-  readxl::read_excel("C:/Users/conno/OneDrive/Desktop/Winter 24/FINTECH 3/natgastables.xlsx", sheet = 6)
+s7 <- s7 %>%  dplyr::mutate(Year =  lubridate::make_date(Year))
 usethis::use_data(s7, overwrite = T)
 
+
 s9 <-  readxl::read_excel("C:/Users/conno/OneDrive/Desktop/Winter 24/FINTECH 3/natgastables.xlsx", sheet = 7)
+s9 <- s9 %>%  dplyr::mutate(Year =  lubridate::make_date(Year))
 usethis::use_data(s9, overwrite = T)
 
+
 s10 <-  readxl::read_excel("C:/Users/conno/OneDrive/Desktop/Winter 24/FINTECH 3/natgastables.xlsx", sheet = 8)
+s10 <- s10 %>%  dplyr::mutate(Year =  lubridate::make_date(Year))
 usethis::use_data(s10, overwrite = T)
 
 
